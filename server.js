f1801130b7b47abd1e9401bd4aa83007469da9af
Juha14 <56065339+Juha14@users.noreply.github.com>
2026-01-29 23:40:21 +0900

    Update server.js

--- server.js
+++ server.js
@@ -3,9 +3,10 @@ const app = express();
 const path = require('path');
 const port = 8080; // App Runner 기본 포트
 
-// 현재 디렉토리의 파일들을 정적 파일로 제공
-app.use(express.static(path.join(__dirname, '.')));
+// 현재 디렉토리의 모든 파일(HTML, CSS, JS)을 정적 파일로 제공
+app.use(express.static(__dirname));
 
+// 루트 경로 접속 시 index.html 전달
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
